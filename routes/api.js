b9c3ebc5403dd235b8f8329e34bebbd4f690164c
Miguel Angel <posadasarraez@gmail.com>
2022-03-14 22:38:38 +0100

    Add catching errors

--- routes/api.js
+++ routes/api.js
@@ -6,11 +6,9 @@ const validate = require('../validation/validate');
 
 // Endpoint to list every entry in the database
 router.get('/list', function (req, res, next) {
-  NewspaperEntry.find({}, function (err, entries) {
-    if (err) return next(err);
-
-    res.json(entries);
-  });
+  NewspaperEntry.find({})
+    .then(event => res.json(event))
+    .catch(err => res.status(400).json('Error: ' + err));
 });
 
 // Endpoint to search some text in the database
@@ -36,40 +34,30 @@ router.get('/', function (req, res, next) {
 // Endpoint to find entries in an id list
 router.get('/:id', function (req, res, next) {
   const idList = req.params.id.split(',');
-  NewspaperEntry.find({ id: { $in: idList } }, function (err, entry) {
-    if (err) return next(err);
-
-    res.json(entry);
-  });
+  NewspaperEntry.find({ id: { $in: idList } })
+    .then(event => res.json(event))
+    .catch(err => res.status(400).json('Error: ' + err));
 });
 
 // Endpoint to create an entry list
 router.post('/', validate.newspaperEntryListSchema, function (req, res, next) {
-  req.body.forEach(element => {
-    NewspaperEntry.create(element, function (err, entry) {
-      if (err) return next(err);
-    });
-  });
-
-  return res.json(req.body);
+  NewspaperEntry.create(req.body)
+    .then(event => res.json(event))
+    .catch(err => res.status(400).json('Error: ' + err));
 });
 
 // Endpoint to update an entry identified by an id
 router.put('/:id', validate.newspaperEntrySchema, function (req, res, next) {
-  NewspaperEntry.findOneAndUpdate({ id: req.params.id }, req.body, { new: true }, function (err, entry) {
-    if (err) return next(err);
-
-    res.json(entry);
-  });
+  NewspaperEntry.findOneAndUpdate({ id: req.params.id }, req.body, { new: true })
+    .then(event => res.json(event))
+    .catch(err => res.status(400).json('Error: ' + err));
 });
 
 // Endpoint to delete an entry identified by an id
 router.delete('/:id', function (req, res, next) {
-  NewspaperEntry.findOneAndDelete({ id: req.params.id }, function (err, entry) {
-    if (err) return next(err);
-
-    res.json(entry);
-  });
+  NewspaperEntry.findOneAndDelete({ id: req.params.id })
+    .then(event => res.json(event))
+    .catch(err => res.status(400).json('Error: ' + err));
 });
 
 module.exports = router;
